a84515e98c6dae0fbb4956f1ece264d9f9d6c9cc
Lenessis <mkorecka@gmail.com>
2022-05-13 00:49:20 +0200

    Init and return files to new project

---
+++ Categories/Add_EditCategory.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TasksList.Category
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Add_EditCategory.xaml
+    /// </summary>
+    public partial class Add_EditCategory : Window
+    {
+        public Add_EditCategory()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Lists/ListDisplay.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TasksList.list
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ListDisplay.xaml
+    /// </summary>
+    public partial class ListDisplay : Window
+    {
+        public ListDisplay()
+        {
+            InitializeComponent();
+        }
+    }
+}
