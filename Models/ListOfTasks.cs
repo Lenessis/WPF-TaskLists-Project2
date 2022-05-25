96f89bbc55f45ae307def453d3313242a282e28f
Lenessis <mkorecka@gmail.com>
2022-05-25 16:09:35 +0200

    Add models

---
+++ Models/ListOfTasks.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksList.Models
+{
+    class ListOfTasks
+    {
+        public List<Task> list { get; set; }
+        public Category category { get; set; }
+        public int urgentState { get; set; }
+
+        /* --- urgentState ---
+         * 
+         *  // 1 -- low
+         *  // 2 -- medium
+         *  // 3 -- high
+         * 
+        */
+
+        public ListOfTasks () 
+        {
+            list = new List<Task>();
+        }
+
+        public ListOfTasks(Category category, int urgent)
+        {
+            list = new List<Task>();
+            this.category = category;
+            this.urgentState = urgent;
+        }
+        public ListOfTasks(string category, int urgent)
+        {
+            list = new List<Task>();
+            this.category = new Category(category);
+            this.urgentState = urgent;
+        }
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksList.Models
+{
+    class Task
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public DateTime date { get; set; }
+        public int urgentState { get; set; }
+        public List<Task> subtasks { get; set; }
+
+        /* --- urgentState ---
+         * 
+         *  // 1 -- low
+         *  // 2 -- medium
+         *  // 3 -- high
+         * 
+        */
+
+        public Task () 
+        {
+            subtasks = new List<Task>();
+        }
+
+        public Task (string name, string desc, DateTime date, int urgent) 
+        {
+            this.name = name;
+            this.description = desc;
+            this.date = date;
+            this.urgentState = urgent;
+            subtasks = new List<Task>();
+        }
+
+        /* --- METHODS --- */
+    }
+}
